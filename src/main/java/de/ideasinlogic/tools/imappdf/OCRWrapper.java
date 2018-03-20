8c6a9986865487db57b41a80bc922433738908b5
Wolfgang Jung <w.jung@polyas.de>
2018-03-20 19:22:34 +0100

    imap-pdf-ocr-2 Add original filename to the scanned files
    Allow multiple files per mail

--- src/main/java/de/ideasinlogic/tools/imappdf/OCRWrapper.java
+++ src/main/java/de/ideasinlogic/tools/imappdf/OCRWrapper.java
@@ -19,32 +19,35 @@ public class OCRWrapper {
    * @param input an input PDF.
    * @param lang  language to use.
    * @return an PDF file with text-annotations, if successful.
-   * @throws IOException          during Process execution or file operations
-   * @throws InterruptedException if interrupted
    */
-  Optional<File> runOCR(File input, String lang) throws IOException, InterruptedException {
-    File output = File.createTempFile("scan-", ".pdf");
-    ProcessBuilder pb = new ProcessBuilder(
-        "ocrmypdf",
-        "-c",
-        "-d",
-        // "--output-type", "pdf",
-        // "--pdf-renderer", "tesseract",
-        "-l", lang,
-        input.getAbsolutePath(),
-        output.getAbsolutePath());
-    pb.inheritIO();
-    Process process = pb.start();
-    log.debug("Started OCR process");
-    int waitFor = process.waitFor();
-    if (waitFor != 0) {
-      log.warn("failed to process " + waitFor);
-      //noinspection ResultOfMethodCallIgnored
-      output.delete();
+  Optional<File> runOCR(File input, String lang) {
+    try {
+      File output = File.createTempFile("scan-", ".pdf");
+      ProcessBuilder pb = new ProcessBuilder(
+          "ocrmypdf",
+          "-c",
+          "-d",
+          // "--output-type", "pdf",
+          // "--pdf-renderer", "tesseract",
+          "-l", lang,
+          input.getAbsolutePath(),
+          output.getAbsolutePath());
+      pb.inheritIO();
+      Process process = pb.start();
+      log.debug("Started OCR process");
+      int waitFor = process.waitFor();
+      if (waitFor != 0) {
+        log.warn("failed to process " + waitFor);
+        //noinspection ResultOfMethodCallIgnored
+        output.delete();
+        return Optional.empty();
+      }
+      log.debug("finished process " + waitFor + " and created file " + output.getAbsolutePath());
+      return Optional.of(output);
+    } catch (InterruptedException | IOException e) {
+      log.debug("failure during processing " + input.getAbsolutePath());
       return Optional.empty();
     }
-    log.debug("finished process " + waitFor + " and created file " + output.getAbsolutePath());
-    return Optional.of(output);
   }
 
 }
